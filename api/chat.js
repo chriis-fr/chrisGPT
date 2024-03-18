fe79a0b7b318fc61421bae28a67196887da1a8a8
christopher odhiambo <caspianodhis@gmail.com>
2024-03-18 13:51:04 +0300

    gott

--- api/chat.js
+++ api/chat.js
@@ -8,7 +8,7 @@ let chat = null;
 module.exports = async (req, res) => {
     try {
         // Ensure the request is a POST request
-        if (req.method !== 'POST') {
+        if (req.method != 'POST') {
             return res.status(405).json({ error: 'Method Not Allowed' });
         }
 
--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT;
 
 // Custom CORS middleware
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://chrisgpt-alpha.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', 'https://chrisgpt-alpha.vercel.app/', 'http://localhost:3000' );
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
